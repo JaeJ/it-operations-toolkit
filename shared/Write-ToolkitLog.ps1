a33b0ab0c22b3a7921fab36d4f4f7788bb7d396c
Jae McNeal <jae.mcneal@gmail.com>
2026-08-24 22:49:23 -0700

    Implement Toolkit v6 file-based logging support

--- shared/Write-ToolkitLog.ps1
+++ shared/Write-ToolkitLog.ps1
@@ -9,8 +9,34 @@ param(
 
 )
 
-$Timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+$ConfigPath = Join-Path `
+    $PSScriptRoot `
+    "..\config\toolkit-config.json"
+
+$Configuration = Get-Content `
+    $ConfigPath |
+    ConvertFrom-Json
+
+if (-not (Test-Path $Configuration.LogPath)) {
+
+    New-Item `
+        -Path $Configuration.LogPath `
+        -ItemType Directory `
+        -Force | Out-Null
+
+}
+
+$LogFile = Join-Path `
+    $Configuration.LogPath `
+    "Toolkit.log"
+
+$Timestamp = Get-Date `
+    -Format "yyyy-MM-dd HH:mm:ss"
 
 $Entry = "[$Timestamp] [$Level] $Message"
 
 Write-Output $Entry
+
+Add-Content `
+    -Path $LogFile `
+    -Value $Entry
